29e91f416f1d6622e37e9c8248259ebc45cb58ef
danilowOENS <dl539994@gmail.com>
2019-10-23 14:39:00 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Projeto-Passa-Pra-Frente
+Repositório separado exclusivamente para Front-End (React)
+
+## Integrantes
+
+- Danilo Owens
+- Julio Cesar
+- Luana Marques
+- Matheus Silva
+- Vanessa da Paixão
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './componentes/CadastroD';
+import CadastroD from './componentes/CadastroD';
+
+const App = () => {
+  return (
+    <CadastroD/>
+  )
+}
+
+
+export default App;
